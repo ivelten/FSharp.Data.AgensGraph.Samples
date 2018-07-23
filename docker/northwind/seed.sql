94ab5bd21a7284f8c16b051d28a0e2cd9813f06b
Ismael Carlos Velten <ismaelcarlosvelten@gmail.com>
2018-07-23 09:56:20 -0300

    Changing database column names to match case sensitive queries

--- docker/northwind/seed.sql
+++ docker/northwind/seed.sql
@@ -12,130 +12,130 @@ SERVER northwind
 OPTIONS (FORMAT 'csv', HEADER 'true', FILENAME '/resources/categories.csv', delimiter ',', quote '"', null '');
 
 CREATE FOREIGN TABLE customers (
-CustomerId char(5),
-CompanyName varchar(40),
-ContactName varchar(30),
-ContactTitle varchar(30),
-Address varchar(60),
-City varchar(15),
-Region varchar(15),
-PostalCode varchar(10),
-Country varchar(15),
-Phone varchar(24),
-Fax varchar(24)
+"customerId" char(5),
+"companyName" varchar(40),
+"contactName" varchar(30),
+"contactTitle" varchar(30),
+"address" varchar(60),
+"city" varchar(15),
+"region" varchar(15),
+"postalCode" varchar(10),
+"country" varchar(15),
+"phone" varchar(24),
+"fax" varchar(24)
 ) 
 SERVER northwind
 OPTIONS (FORMAT 'csv', HEADER 'true', FILENAME '/resources/customers.csv', delimiter ',', quote '"', null '');
 
 CREATE FOREIGN TABLE employees (
-EmployeeId int,
-LastName varchar(20),
-FirstName varchar(10),
-Title varchar(30),
-TitleOfCourtesy varchar(25),
-BirthDate date,
-HireDate date,
-Address varchar(60),
-City varchar(15),
-Region varchar(15),
-PostalCode varchar(10),
-Country varchar(15),
-HomePhone varchar(24),
-Extension varchar(4),
-Notes text,
-ReportTo int,
-PhotoPath varchar(255)
+"employeeId" int,
+"lastName" varchar(20),
+"firstName" varchar(10),
+"title" varchar(30),
+"titleOfCourtesy" varchar(25),
+"birthDate" date,
+"hireDate" date,
+"address" varchar(60),
+"city" varchar(15),
+"region" varchar(15),
+"postalCode" varchar(10),
+"country" varchar(15),
+"homePhone" varchar(24),
+"extension" varchar(4),
+"notes" text,
+"reportTo" int,
+"photoPath" varchar(255)
 ) 
 SERVER northwind 
 OPTIONS (FORMAT 'csv', HEADER 'true', FILENAME '/resources/employees.csv', delimiter ',', quote '"', null '');
 
 CREATE FOREIGN TABLE employee_territories (
-EmployeeId int,
-TerritoryId varchar(20)
+"employeeId" int,
+"territoryId" varchar(20)
 ) 
 SERVER northwind
 OPTIONS (FORMAT 'csv', HEADER 'true', FILENAME '/resources/employee_territories.csv', delimiter ',', quote '"', null ''); 
 
 CREATE FOREIGN TABLE orders_details (
-orderId int,
-ProductId int,
-UnitPrice numeric(18,2),
-Quantity smallint,
-Discount real
+"orderId" int,
+"productId" int,
+"unitPrice" numeric(18,2),
+"quantity" smallint,
+"discount" real
 ) 
 SERVER northwind
 OPTIONS (FORMAT 'csv', HEADER 'true', FILENAME '/resources/orders_details.csv', delimiter ',', quote '"', null ''); 
 
 CREATE FOREIGN TABLE orders (
-orderId int,
-CustomerId char(5),
-EmployeeId int,
-orderDate date,
-RequiredDate date,
-ShippedDate date,
-ShipVia int,
-Freight numeric(18,2),
-ShipName varchar(40),
-ShipAddress varchar(60),
-ShipCity varchar(15),
-ShipRegion varchar(15), 
-ShipPostalCode varchar(10), 
-ShipCountry varchar(15) 
+"orderId" int,
+"customerId" char(5),
+"employeeId" int,
+"orderDate" date,
+"requiredDate" date,
+"shippedDate" date,
+"shipVia" int,
+"freight" numeric(18,2),
+"shipName" varchar(40),
+"shipAddress" varchar(60),
+"shipCity" varchar(15),
+"shipRegion" varchar(15), 
+"shipPostalCode" varchar(10), 
+"shipCountry" varchar(15) 
 ) 
 SERVER northwind 
 OPTIONS (FORMAT 'csv', HEADER 'true', FILENAME '/resources/orders.csv', delimiter ',', quote '"', null ''); 
 
 CREATE FOREIGN TABLE products (
-ProductId int, 
-ProductName varchar(40),
-SupplierId int, 
-CategoryId int, 
-QuantityPerUnit varchar(20),
-UnitPrice numeric(18,2), 
-UnitsInStock smallint, 
-UnitsOnorder smallint, 
-ReorderLevel smallint, 
-Discontinued bit 
+"productId" int, 
+"productName" varchar(40),
+"supplierId" int, 
+"categoryId" int, 
+"quantityPerUnit" varchar(20),
+"unitPrice" numeric(18,2), 
+"unitsInStock" smallint, 
+"unitsOnOrder" smallint, 
+"reorderLevel" smallint, 
+"discontinued" bit 
 ) 
 SERVER northwind
 OPTIONS (FORMAT 'csv', HEADER 'true', FILENAME '/resources/products.csv', delimiter ',', quote '"', null '');
 
 CREATE FOREIGN TABLE regions (
-RegionId int,
-RegionDescription char(50)
+"regionId" int,
+"regionDescription" char(50)
 ) 
 SERVER northwind
 OPTIONS (FORMAT 'csv', HEADER 'true', FILENAME '/resources/regions.csv', delimiter ',', quote '"', null ''); 
 
 CREATE FOREIGN TABLE shippers (
-ShipperId int,
-CompanyName varchar(40),
-Phone varchar(24)
+"shipperId" int,
+"companyName" varchar(40),
+"phone" varchar(24)
 ) 
 SERVER northwind
 OPTIONS (FORMAT 'csv', HEADER 'true', FILENAME '/resources/shippers.csv', delimiter ',', quote '"', null ''); 
 
 CREATE FOREIGN TABLE suppliers (
-SupplierId int,
-CompanyName varchar(40),
-ContactName varchar(30),
-ContactTitle varchar(30),
+"supplierId" int,
+"companyName" varchar(40),
+"contactName" varchar(30),
+"contactTitle" varchar(30),
 Address varchar(60),
-City varchar(15),
-Region varchar(15),
-PostalCode varchar(10),
-Country varchar(15),
-Phone varchar(24),
-Fax varchar(24),
-HomePage text
+"city" varchar(15),
+"region" varchar(15),
+"postalCode" varchar(10),
+"country" varchar(15),
+"phone" varchar(24),
+"fax" varchar(24),
+"homePage" text
 ) 
 SERVER northwind
 OPTIONS (FORMAT 'csv', HEADER 'true', FILENAME '/resources/suppliers.csv', delimiter ',', quote '"', null '');
 
 CREATE FOREIGN TABLE territories (
-TerritoryId varchar(20),
-TerritoryDescription char(50),
-RegionId int
+"territoryId" varchar(20),
+"territoryDescription" char(50),
+"regionId" int
 ) 
 SERVER northwind
 OPTIONS (FORMAT 'csv', HEADER 'true', FILENAME '/resources/territories.csv', delimiter ',', quote '"', null '');
@@ -154,40 +154,40 @@ LOAD FROM shippers AS source CREATE (n:shipper=to_jsonb(source));
 LOAD FROM suppliers AS source CREATE (n:supplier=to_jsonb(source));
 LOAD FROM territories AS source CREATE (n:territory=to_jsonb(source));
 
-CREATE PROPERTY INDEX ON category(categoryid);
-CREATE PROPERTY INDEX ON customer(customerid);
-CREATE PROPERTY INDEX ON employee(employeeid);
-CREATE PROPERTY INDEX ON "order"(orderid);
-CREATE PROPERTY INDEX ON product(productid);
-CREATE PROPERTY INDEX ON region(regionid);
-CREATE PROPERTY INDEX ON shipper(shipperid);
-CREATE PROPERTY INDEX ON supplier(supplierid);
-CREATE PROPERTY INDEX ON territory(territoryid);
+CREATE PROPERTY INDEX ON category("categoryId");
+CREATE PROPERTY INDEX ON customer("customerId");
+CREATE PROPERTY INDEX ON employee("employeeId");
+CREATE PROPERTY INDEX ON "order"("orderId");
+CREATE PROPERTY INDEX ON product("productId");
+CREATE PROPERTY INDEX ON region("regionId");
+CREATE PROPERTY INDEX ON shipper("shipperId");
+CREATE PROPERTY INDEX ON supplier("supplierId");
+CREATE PROPERTY INDEX ON territory("territoryId");
 
 LOAD FROM orders_details AS source
 MATCH (n:"order"),(m:product)
-WHERE n.orderid=to_jsonb((source).orderid)
-AND m.productid=to_jsonb((source).productid)
-CREATE (n)-[r:ORDERS {unitprice:(source).unitprice,quantity:(source).quantity,discount:(source).discount}]->(m);
+WHERE n."orderId"=to_jsonb((source)."orderId")
+AND m."productId"=to_jsonb((source)."productId")
+CREATE (n)-[r:ORDERS {"unitPrice":(source)."unitPrice","quantity":(source)."quantity","discount":(source)."discount"}]->(m);
 
 MATCH (n:employee),(m:employee)
-WHERE m.employeeid=n.reportto
+WHERE m."employeeId"=n."reportTo"
 CREATE (n)-[r:REPORTS_TO]->(m);
 
 MATCH (n:supplier),(m:product)
-WHERE m.supplierid=n.supplierid
+WHERE m."supplierId"=n."supplierId"
 CREATE (n)-[r:SUPPLIES]->(m);
 
 MATCH (n:product),(m:category)
-WHERE n.categoryid=m.categoryid
+WHERE n."categoryId"=m."categoryId"
 CREATE (n)-[r:PART_OF]->(m);
 
 MATCH (n:customer),(m:"order")
-WHERE m.customerid=n.customerid
+WHERE m."customerId"=n."customerId"
 CREATE (n)-[r:PURCHASED]->(m);
 
 MATCH (n:employee),(m:"order")
-WHERE m.employeeid=n.employeeid
+WHERE m."employeeId"=n."employeeId"
 CREATE (n)-[r:SOLD]->(m);
 
 DO $$
@@ -214,4 +214,4 @@ WITH c, count(p) AS total
 MATCH (c)-[:PURCHASED]->(o:"order")-[:ORDERS]->(p:product)
 WITH c, total, p, count(o) AS orders
 WITH c, total, p, orders, orders*1.0/total AS rating
-MERGE (c)-[rated:RATED {totalcount: to_jsonb(total), ordercount: to_jsonb(orders), rating: rating}]->(p);
+MERGE (c)-[rated:RATED {"totalCount": to_jsonb(total), "orderCount": to_jsonb(orders), rating: rating}]->(p);
